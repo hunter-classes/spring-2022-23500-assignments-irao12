841a370187f2f27c221cd447e5eeff0e0d069cd0
Ivan Rao <ivanrao@Ivans-MacBook-Air.local>
2022-02-28 17:47:20 -0500

    added tests for constructor, insert, and toString

--- assignments/linked_list/tests.cpp
+++ assignments/linked_list/tests.cpp
@@ -4,3 +4,16 @@
 #include "OList.h"
 
 OList * list = new OList();
+TEST_CASE("Testing constructor, insert, and toString"){
+  CHECK(list->toString() == "nullptr");
+  list->insert(10);
+  CHECK(list->toString() == "10-->nullptr");
+  list->insert(12);
+  CHECK(list->toString() == "10-->12-->nullptr");
+  list->insert(5);
+  CHECK(list->toString() == "5-->10-->12-->nullptr");
+  list->insert(-11);
+  CHECK(list->toString() == "-11-->5-->10-->12-->nullptr");
+  list->insert(7);
+  CHECK(list->toString() == "-11-->5-->7-->10-->12-->nullptr");
+}
