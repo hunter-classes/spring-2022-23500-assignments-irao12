b9bb783c4242f54a66b1aeae05927ff1762f72ff
Ivan Rao <ivanrao@Ivans-MacBook-Air.local>
2022-02-28 17:59:26 -0500

    added destructor and contains

--- assignments/linked_list/OList.cpp
+++ assignments/linked_list/OList.cpp
@@ -4,26 +4,39 @@
 OList::OList(){
   head = nullptr;
 }
-//~OList();
+
+OList::~OList(){
+  Node * trailer = nullptr;
+  while (head != nullptr){
+    trailer = head;
+    head = head->getNext();
+    delete trailer;
+  }
+}
 
 void OList::insert(int value){
+  //while walker refers to index n, trailer refers to index n-1
   Node *walker = head;
   Node *trailer = nullptr;
   Node *temp = new Node(value);
 
+  //walker must not be nullptr and value must be greater
   while (walker != nullptr && value > walker->getData()){
     trailer = walker;
     walker = walker->getNext();
   }
 
+  //inserting at index 0
   if (trailer == nullptr){
     temp->setNext(head);
     head = temp;
   }
+  //inserting elsewhere
   else{
     trailer->setNext(temp);
     temp->setNext(walker);
   }
+
   return;
 }
 
@@ -40,7 +53,18 @@ std::string OList::toString(){
   return result;
 }
 
-//bool contains(int value);
+bool OList::contains(int value){
+  Node *walker = head;
+  //skip Nodes until walker has data >= value
+  while (walker != nullptr && walker->getData() < value){
+    walker = walker->getNext();
+  }
+
+  if (walker == nullptr || walker->getData() > value){
+    return false;
+  }
+  return true;
+}
 
 //int get(int loc);
 
