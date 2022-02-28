d7c4c98ce26d7112323ad21c36e66f0011469b1a
Ivan Rao <ivanrao@Ivans-MacBook-Air.local>
2022-02-28 17:59:42 -0500

    added tests for contain

--- assignments/linked_list/tests.cpp
+++ assignments/linked_list/tests.cpp
@@ -3,8 +3,9 @@
 #include <iostream>
 #include "OList.h"
 
-OList * list = new OList();
+
 TEST_CASE("Testing constructor, insert, and toString"){
+  OList * list = new OList();
   CHECK(list->toString() == "nullptr");
   list->insert(10);
   CHECK(list->toString() == "10-->nullptr");
@@ -16,4 +17,22 @@ TEST_CASE("Testing constructor, insert, and toString"){
   CHECK(list->toString() == "-11-->5-->10-->12-->nullptr");
   list->insert(7);
   CHECK(list->toString() == "-11-->5-->7-->10-->12-->nullptr");
+  delete list;
+}
+
+TEST_CASE("testing contains"){
+  OList * list = new OList();
+  //testing on an empty list
+  CHECK(!list->contains(5)); //false
+  list->insert(10);
+  list->insert(12);
+  list->insert(5);
+  CHECK(list->contains(10));
+  CHECK(list->contains(12));
+  CHECK(list->contains(5));
+  //not yet inserted
+  CHECK(!list->contains(4));
+  list->insert(4);
+  CHECK(list->contains(4));
+  delete list;
 }
