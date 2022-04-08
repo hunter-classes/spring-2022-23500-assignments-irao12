e748ceb58d843d01e432262ff38712456e238698
Ivan Rao <ivanrao@Ivans-MacBook-Air.local>
2022-04-07 22:35:39 -0400

    implemented Stack using Node

---
+++ assignments/stacks_queues/Stack.cpp
@@ -0,0 +1,34 @@
+#include "Stack.h"
+
+Stack::Stack(){
+  stack = nullptr;
+}
+
+Stack::~Stack(){
+  delete stack;
+}
+
+void Stack::push(int val){
+  Node * temp = new Node(val, stack);
+  stack = temp;
+  return;
+}
+
+int Stack::pop(){
+  if (stack == nullptr) throw ERR_STACK_EMPTY;
+  int val = stack->getData();
+  Node * removing = stack;
+  stack = stack->getNext();
+  delete removing;
+  return val;
+}
+
+int Stack::top(){
+  if (stack == nullptr) throw ERR_STACK_EMPTY;
+  return stack->getData();
+}
+
+bool Stack::is_empty(){
+  if (stack == nullptr) return true;
+  return false;
+}
---
+++ assignments/stacks_queues/Stack.h
@@ -0,0 +1,15 @@
+#include "Node.h"
+
+#define ERR_STACK_EMPTY 1
+
+class Stack{
+public:
+  Stack();
+  ~Stack();
+  void push(int val);
+  int pop();
+  int top();
+  bool is_empty();
+private:
+  Node * stack;
+};
